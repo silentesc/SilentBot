ec7eaf6a26653ea021c69978b65b4b77656f5be4
silentesc <dominic.woehrle@gmail.com>
2024-08-03 04:46:18 +0200

    Message now doesn't show who used the command

--- commands/reaction_role_command.py
+++ commands/reaction_role_command.py
@@ -5,6 +5,8 @@ from utils import storage_manager
 
 
 async def on_reaction_role(client: discord.Client, interaction: discord.Interaction, message_text: str, label: str, role: discord.Role) -> None:
+    await interaction.response.defer()
+
     db = storage_manager.SQLiteManager()
     await db.connect()
     custom_id = await db.execute_one("INSERT INTO reaction_roles (label, style, role_id, guild_id) VALUES (?, ?, ?, ?) RETURNING id", label,  discord.ButtonStyle.primary.value, role.id, interaction.guild_id)
@@ -13,4 +15,5 @@ async def on_reaction_role(client: discord.Client, interaction: discord.Interact
 
     view = reaction_roles.ReactionRoles(label=label, custom_id=str(custom_id[0]), style=discord.ButtonStyle.primary, role=role)
 
-    await interaction.response.send_message(message_text, view=view)
+    await interaction.channel.send(message_text, view=view)
+    await interaction.delete_original_response()
